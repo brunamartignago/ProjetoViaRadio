2debe8790bc911fdd367b40e92b1451e8432ef75
Bruna <Bruna@Bruna-PC>
2014-07-30 17:57:23 -0300

    Classe cliente rede controller do projeto via rádio.

--- src/br/senai/sc/controller/ClienteRedeFaturaController.java
+++ src/br/senai/sc/controller/ClienteRedeFaturaController.java
@@ -1,13 +1,21 @@
 package br.senai.sc.controller;
-
 import br.senai.sc.model.negocio.ClienteRedeFatura;
 import br.senai.sc.model.persistencia.ClienteRedeFaturaDAOJPA;
 import br.senai.sc.model.persistencia.dao.ClienteRedeFaturaDAO;
 import java.util.List;
 
 /**
+ * Classe ClienteRedeFaturaController, nessa classe possui os métodos de inserir, editar,
+ * salvar, listar todos, listar por ID e pesquisar, da determinada classe. Essa
+ * classe cria um construtor, e insere o nome da classe dentro do parentese, na
+ * próxima linha chama-se a classe DAO da determinada classe, e ao lado cria um
+ * construtor chamando a classe DAOJPA também. Na última linha insere-se um
+ * retorno com o dao.save para salvar a classe que está sendo inserida, assim
+ * sucessivamente.
  *
- * @author Bruna
+ * @author Bruna Zakrzeski
+ * @since 1.0
+ * @version 1.0 07/08/2014
  */
 
 public class ClienteRedeFaturaController {
