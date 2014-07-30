15586ad42fe263354839babb14d72d9cb8c3c92d
Bruna <Bruna@Bruna-PC>
2014-07-30 18:04:58 -0300

    Classe status recebimento controller do projeto via rádio.

--- src/br/senai/sc/controller/StatusRecebimentoController.java
+++ src/br/senai/sc/controller/StatusRecebimentoController.java
@@ -1,5 +1,4 @@
 package br.senai.sc.controller;
-
 import br.senai.sc.model.negocio.StatusRecebimento;
 import br.senai.sc.model.persistencia.StatusRecebimentoDAOJPA;
 import br.senai.sc.model.persistencia.dao.StatusRecebimentoDAO;
@@ -7,8 +6,17 @@ import java.util.List;
 
 
 /**
+ * Classe StatusRecebimentoController, nessa classe possui os métodos de inserir, editar,
+ * salvar, listar todos, listar por ID e pesquisar, da determinada classe. Essa
+ * classe cria um construtor, e insere o nome da classe dentro do parentese, na
+ * próxima linha chama-se a classe DAO da determinada classe, e ao lado cria um
+ * construtor chamando a classe DAOJPA também. Na última linha insere-se um
+ * retorno com o dao.save para salvar a classe que está sendo inserida, assim
+ * sucessivamente.
  *
- * @author Bruna
+ * @author Bruna Zakrzeski
+ * @since 1.0
+ * @version 1.0 07/08/2014
  */
 
 public class StatusRecebimentoController {
